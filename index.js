525ee1cfc2d1def3b00764a59b3241222b88d134
Shuo Wang <gken90@gmail.com>
2019-01-09 21:36:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(driver => {
+    console.log(driver.name)
+  });
+}
+
+function logDriverByHometown(drivers, hometown) {
+   drivers.forEach(driver => {
+    if (driver.hometown === hometown){return driver.name};
+  })
+}
